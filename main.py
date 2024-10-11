68eb1c731366cf9005461cd130544d0bc596b354
General Team <46902785+Boga28@users.noreply.github.com>
2024-10-11 10:41:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -200,5 +200,5 @@ if __name__ == '__main__':
   # app.run(debug=True, host="0.0.0.0", port="5000")
   # Production
   #app.run(host="0.0.0.0",  port=81)
-  http_server = WSGIServer(('', 8080), app)
+  http_server = WSGIServer(('', 5000), app)
   http_server.serve_forever()
